e78a450492616951228e3407e4621db20d53bb43
Fiusion <92926501+StefanoPoropat@users.noreply.github.com>
2023-09-26 22:31:14 +0200

    Add files via upload

---
+++ determinantaTrecegReda/determinantaTrecegReda/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace determinantaTrecegReda
+{
+    partial class KalkulatorDeterminanteTrećegReda
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtA11 = new System.Windows.Forms.TextBox();
+            this.txtA12 = new System.Windows.Forms.TextBox();
+            this.txtA13 = new System.Windows.Forms.TextBox();
+            this.txtA21 = new System.Windows.Forms.TextBox();
+            this.txtA22 = new System.Windows.Forms.TextBox();
+            this.txtA23 = new System.Windows.Forms.TextBox();
+            this.txtA31 = new System.Windows.Forms.TextBox();
+            this.txtA32 = new System.Windows.Forms.TextBox();
+            this.txtA33 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTEXT = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lblD = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox1.Image = global::determinantaTrecegReda.Properties.Resources.Bez_naslova3_removebg_preview;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1049, 450);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(12, 307);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(236, 70);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtA11
+            // 
+            this.txtA11.ForeColor = System.Drawing.Color.Black;
+            this.txtA11.Location = new System.Drawing.Point(275, 86);
+            this.txtA11.Name = "txtA11";
+            this.txtA11.Size = new System.Drawing.Size(100, 22);
+            this.txtA11.TabIndex = 2;
+            this.txtA11.Text = "0";
+            this.txtA11.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtA12
+            // 
+            this.txtA12.ForeColor = System.Drawing.Color.Black;
+            this.txtA12.Location = new System.Drawing.Point(418, 86);
+            this.txtA12.Name = "txtA12";
+            this.txtA12.Size = new System.Drawing.Size(100, 22);
+            this.txtA12.TabIndex = 3;
+            this.txtA12.Text = "0";
+            this.txtA12.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtA13
+            // 
+            this.txtA13.ForeColor = System.Drawing.Color.Black;
+            this.txtA13.Location = new System.Drawing.Point(572, 86);
+            this.txtA13.Name = "txtA13";
+            this.txtA13.Size = new System.Drawing.Size(100, 22);
+            this.txtA13.TabIndex = 4;
+            this.txtA13.Text = "0";
+            this.txtA13.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtA21
+            // 
+            this.txtA21.ForeColor = System.Drawing.Color.Black;
+            this.txtA21.Location = new System.Drawing.Point(275, 199);
+            this.txtA21.Name = "txtA21";
+            this.txtA21.Size = new System.Drawing.Size(100, 22);
+            this.txtA21.TabIndex = 5;
+            this.txtA21.Text = "0";
+            this.txtA21.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtA22
+            // 
+            this.txtA22.ForeColor = System.Drawing.Color.Black;
+            this.txtA22.Location = new System.Drawing.Point(418, 199);
+            this.txtA22.Name = "txtA22";
+            this.txtA22.Size = new System.Drawing.Size(100, 22);
+            this.txtA22.TabIndex = 6;
+            this.txtA22.Text = "0";
+            this.txtA22.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtA23
+            // 
+            this.txtA23.ForeColor = System.Drawing.Color.Black;
+            this.txtA23.Location = new System.Drawing.Point(572, 199);
+            this.txtA23.Name = "txtA23";
+            this.txtA23.Size = new System.Drawing.Size(100, 22);
+            this.txtA23.TabIndex = 7;
+            this.txtA23.Text = "0";
+            this.txtA23.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtA31
+            // 
+            this.txtA31.ForeColor = System.Drawing.Color.Black;
+            this.txtA31.Location = new System.Drawing.Point(275, 307);
+            this.txtA31.Name = "txtA31";
+            this.txtA31.Size = new System.Drawing.Size(100, 22);
+            this.txtA31.TabIndex = 8;
+            this.txtA31.Text = "0";
+            this.txtA31.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtA32
+            // 
+            this.txtA32.ForeColor = System.Drawing.Color.Black;
+            this.txtA32.Location = new System.Drawing.Point(418, 307);
+            this.txtA32.Name = "txtA32";
+            this.txtA32.Size = new System.Drawing.Size(100, 22);
+            this.txtA32.TabIndex = 9;
+            this.txtA32.Text = "0";
+            this.txtA32.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtA33
+            // 
+            this.txtA33.ForeColor = System.Drawing.Color.Black;
+            this.txtA33.Location = new System.Drawing.Point(572, 307);
+            this.txtA33.Name = "txtA33";
+            this.txtA33.Size = new System.Drawing.Size(100, 22);
+            this.txtA33.TabIndex = 10;
+            this.txtA33.Text = "0";
+            this.txtA33.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(1049, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "label1";
+            // 
+            // lblTEXT
+            // 
+            this.lblTEXT.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblTEXT.Location = new System.Drawing.Point(96, 185);
+            this.lblTEXT.Name = "lblTEXT";
+            this.lblTEXT.Size = new System.Drawing.Size(121, 56);
+            this.lblTEXT.TabIndex = 12;
+            this.lblTEXT.Text = "D = ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(300, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 20);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "A11";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(447, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 20);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "A12";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(600, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 20);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "A13";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(600, 163);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 20);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "A23";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(447, 163);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 20);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "A22";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(300, 163);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 20);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "A21";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.Location = new System.Drawing.Point(600, 268);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 20);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "A33";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.Location = new System.Drawing.Point(447, 268);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(38, 20);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "A32";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label10.Location = new System.Drawing.Point(300, 268);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(38, 20);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "A31";
+            // 
+            // lblD
+            // 
+            this.lblD.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblD.ForeColor = System.Drawing.Color.Black;
+            this.lblD.Location = new System.Drawing.Point(340, 385);
+            this.lblD.Name = "lblD";
+            this.lblD.Size = new System.Drawing.Size(242, 56);
+            this.lblD.TabIndex = 22;
+            this.lblD.Text = "D = ";
+            this.lblD.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // KalkulatorDeterminanteTrećegReda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblD);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblTEXT);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtA33);
+            this.Controls.Add(this.txtA32);
+            this.Controls.Add(this.txtA31);
+            this.Controls.Add(this.txtA23);
+            this.Controls.Add(this.txtA22);
+            this.Controls.Add(this.txtA21);
+            this.Controls.Add(this.txtA13);
+            this.Controls.Add(this.txtA12);
+            this.Controls.Add(this.txtA11);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "KalkulatorDeterminanteTrećegReda";
+            this.Text = "Kalkulator Determinante Trećeg Reda";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtA11;
+        private System.Windows.Forms.TextBox txtA12;
+        private System.Windows.Forms.TextBox txtA13;
+        private System.Windows.Forms.TextBox txtA21;
+        private System.Windows.Forms.TextBox txtA22;
+        private System.Windows.Forms.TextBox txtA23;
+        private System.Windows.Forms.TextBox txtA31;
+        private System.Windows.Forms.TextBox txtA32;
+        private System.Windows.Forms.TextBox txtA33;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTEXT;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lblD;
+    }
+}
+
---
+++ determinantaTrecegReda/determinantaTrecegReda/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace determinantaTrecegReda
+{
+    public partial class KalkulatorDeterminanteTrećegReda : Form
+    {
+        public KalkulatorDeterminanteTrećegReda()
+        {
+            InitializeComponent();
+        }
+        float a11=0, a12=0, a13=0, a21 = 0, a22 = 0, a23 = 0, a31 = 0, a32 = 0, a33 = 0;
+
+        float rez = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rez = 0;
+            a11 = (int)Convert.ToSingle(txtA11.Text);
+            a12 = (int)Convert.ToSingle(txtA12.Text);
+            a13 = (int)Convert.ToSingle(txtA13.Text);
+//-----------------------------------------------------------------------
+            a21 = (int)Convert.ToSingle(txtA21.Text);
+            a22 = (int)Convert.ToSingle(txtA22.Text);
+            a23 = (int)Convert.ToSingle(txtA23.Text);
+//-----------------------------------------------------------------------
+            a31 = (int)Convert.ToSingle(txtA31.Text);
+            a32 = (int)Convert.ToSingle(txtA32.Text);
+            a33 = (int)Convert.ToSingle(txtA33.Text);
+            //-----------------------------------------------------------------------
+            rez = a11 * a22 * a33 + a12 * a23 * a31 + a13 * a21 * a32 - a31 * a22 * a13 - a32 * a23 * a11 - a33 * a21 * a12;
+            lblD.Text = "D = " + rez;
+        }
+    }
+}
